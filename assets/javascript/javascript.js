989d1bfcaa27e1bf722d10b192bb155ddb0fc3cd
packleader206 <packleader206@gmail.com>
2018-11-13 08:26:38 -0800

    basic layout html, javascript firebase setup

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,20 @@
+//Javascript for Train-Scheduler - Week 7 Homework Assignment - UW Coding Bootcamp
+
+$(document).ready(function(){
+
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAlFrxfNwtynGE24GyHzh3g_WT7P_cpng4",
+        authDomain: "train-scheduler-6d7f0.firebaseapp.com",
+        databaseURL: "https://train-scheduler-6d7f0.firebaseio.com",
+        projectId: "train-scheduler-6d7f0",
+        storageBucket: "train-scheduler-6d7f0.appspot.com",
+        messagingSenderId: "867039645149"
+    };
+    firebase.initializeApp(config);
+
+    
+});
+
+
