78926312aefeb4bbe17da9422bcea9deb5d43983
packleader206 <packleader206@gmail.com>
2018-11-14 08:54:11 -0800

    fixed typos, added code block for push from firebase DB

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,21 +3,24 @@
 $(document).ready(function(){
 
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAlFrxfNwtynGE24GyHzh3g_WT7P_cpng4",
-        authDomain: "train-scheduler-6d7f0.firebaseapp.com",
-        databaseURL: "https://train-scheduler-6d7f0.firebaseio.com",
-        projectId: "train-scheduler-6d7f0",
-        storageBucket: "train-scheduler-6d7f0.appspot.com",
-        messagingSenderId: "867039645149"
-    };
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAlFrxfNwtynGE24GyHzh3g_WT7P_cpng4",
+    authDomain: "train-scheduler-6d7f0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6d7f0.firebaseio.com",
+    projectId: "train-scheduler-6d7f0",
+    storageBucket: "train-scheduler-6d7f0.appspot.com",
+    messagingSenderId: "867039645149"
+  };
+
     firebase.initializeApp(config);
+    console.log(firebase);
 
     let database = firebase.database();
 
+
     //event listener for user click of 'Add Train' button. Grabs user input values and adds data to firebase
-    $("#addTrain").on("click", function (event) {
+    $("#addTrain").on("click", function(event) {
     event.preventDefault();
 
         //grab input values
@@ -31,13 +34,37 @@ $(document).ready(function(){
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
-            frequency: frequency,
+            frequency: frequency
         });
+        $("#userInputForm").trigger("reset");
+    });
+
+    //firebase event listener for new data. When new data is added to the database, function runs accordingly
+    database.ref().on("child_added", function(childSnapshot) {
+
+        let newTrain = childSnapshot.val().trainName;
+        let newDestination = childSnapshot.val().destination;
+        let newFirstTrainTime = childSnapshot.val().firstTrainTime;
+        let newFrequency = childSnapshot.val().frequency;
+
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrainTime);
+        console.log(frequency);
+
+        $("#tableBody").append (
+            "<tr><td>" + newTrain +
+            "</td><td>" + newDestination +
+            "</td><td>" + newFrequency +
+            "</td><td>" + "" +
+            "</td><td>" + "" +
+            "</td><td>" + "x" + "</td></tr>");
+    },
+    function (errorObject) {
+        console.log("Errors handeled: " + errorObject.code);
     });
 
     
-
-
 });
 
 
